23795cf7086550ae618908cacec2c14a5df59d33
boura86 <bouradiallo86@gmail.com>
2018-11-14 19:49:00 +0100

    first commit add project tlc

---
+++ src/main/java/com/tlc/tlc/TlcApplication.java
@@ -0,0 +1,12 @@
+package com.tlc.tlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlcApplication.class, args);
+    }
+}
